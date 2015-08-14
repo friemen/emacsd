fa026549ab7ab1d133a813e18000af25c3742668
Falko Riemenschneider <riemenschneider@doctronic.de>
2015-08-14 07:08:34 +0200

    ivy-mode confuses me when switching buffers

--- modules/11-misc.el
+++ modules/11-misc.el
@@ -200,12 +200,12 @@
 
 
 ;; swiper
-(ivy-mode 1)
-(setq ivy-use-virtual-buffers t)
+(require 'swiper)
+;; (ivy-mode 1)
+;; (setq ivy-use-virtual-buffers t)
 (global-set-key "\C-s" 'swiper)
 (global-set-key "\C-r" 'swiper)
 (global-set-key (kbd "C-c C-r") 'ivy-resume)
-(global-set-key [f6] 'ivy-resume)
 
 
 ;; switch window configuration
