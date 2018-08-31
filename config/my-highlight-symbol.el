c0b6cc03625a2bf08d6e3981c6aa8e75ac14419d
Falko Riemenschneider <riemenschneider@doctronic.de>
2018-08-31 16:50:12 +0200

    Don't display number of occurrences

--- config/my-highlight-symbol.el
+++ config/my-highlight-symbol.el
@@ -4,3 +4,9 @@
  				  "Green Yellow" "Light Salmon"))
 (setq highlight-symbol-idle-delay 0.7)
 (setq highlight-symbol-should-auto-highlight-p 1)
+
+
+(defun highlight-symbol-count ()
+  ;; intentionally do nothing
+  ;; so that "n occurrences in buffer" doesn't replace useful infos in minibuffer
+  )
