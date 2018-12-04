74b8dcf6b1224e607f316734c5bcd3cdc84f521e
Falko Riemenschneider <riemenschneider@doctronic.de>
2018-12-04 14:16:04 +0100

    Use other projectile indexing method b/c native didn't work anymore

--- config/my-projectile.el
+++ config/my-projectile.el
@@ -3,7 +3,7 @@
 
 (projectile-mode 1)
 (setq projectile-switch-project-action 'my-neotree-show)
-(setq projectile-indexing-method 'native)
+(setq projectile-indexing-method 'alien)
 (setq projectile-enable-caching t)
 (setq projectile-globally-ignored-directories
       (append '( "out" "target" ".cljs_rhino_repl" ".git")
