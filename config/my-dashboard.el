1297b3b71c01577cd45718737e8498b555b9f0f3
Falko Riemenschneider <riemenschneider@doctronic.de>
2019-09-10 22:24:39 +0200

    Properly setup dashboard

--- config/my-dashboard.el
+++ config/my-dashboard.el
@@ -3,10 +3,11 @@
 (require-packages '(dashboard))
 
 (dashboard-setup-startup-hook)
-
-(setq dashboard-items '((recents  . 5)
+(setq dashboard-startup-banner 'logo)
+(setq dashboard-banner-logo-title "Welcome to Emacs!")
+(setq dashboard-items '((projects . 15)
+			(recents  . 10)
                         (bookmarks . 5)
-                        (projects . 5)
                         ;;(agenda . 5)
                         ;;(registers . 5)
 			))
