fc539b1bb6db4a860b5e19958a0877567c55f59d
Falko Riemenschneider <riemenschneider@doctronic.de>
2021-10-02 16:22:14 +0200

    Add fn and binding for buffer indent

--- config/my-web-css-sass.el
+++ config/my-web-css-sass.el
@@ -1,35 +1,5 @@
 (provide 'my-web-css-sass)
 
-(use-package web-mode :ensure t
-  :mode "\\.html?\\'"
-
-  :bind
-  (:map web-mode-map
-        ("C-7" . web-mode-element-close)
-        ("C-c C-e l" . my-web-mode-insert-link)
-        ("C-c x x" . web-mode-fold-or-unfold)
-        ("C-SPC" . web-mode-mark-and-expand))
-
-  :config
-  (setq web-mode-indent-style 2)
-  (setq web-mode-markup-indent-offset 2)
-  (add-hook 'web-mode-hook 'company-mode)
-  )
-
-(use-package css-mode :ensure t
-  :config
-  (add-hook 'css-mode-hook 'company-mode))
-
-(use-package sass-mode :ensure t
-  :mode "\\.sass\\'"
-  :config
-  (add-hook 'sass-mode-hook 'company-mode))
-
-(use-package scss-mode :ensure t
-  :mode "\\.scss\\'"
-  :config
-  (add-hook 'scss-mode-hook 'company-mode))
-
 
 (defun my-web-mode-insert-link (href)
   (interactive "sInsert link with HREF: ")
@@ -41,3 +11,48 @@
 		   "</a>"))
     (delete-active-region)
     (insert el)))
+
+(defun my-web-mode-indent-buffer ()
+  (interactive)
+  (mark-whole-buffer)
+  (indent-region (point-min) (point-max) nil)
+  (pop-global-mark))
+
+
+(use-package web-mode :ensure t
+  :mode "\\.html?\\'"
+  :bind
+  (:map web-mode-map
+        ("C-c C-i" . my-web-mode-indent-buffer)
+        ("C-7" . web-mode-element-close)
+        ("C-c C-e l" . my-web-mode-insert-link)
+        ("C-c x x" . web-mode-fold-or-unfold)
+        ("C-SPC" . web-mode-mark-and-expand))
+
+  :config
+  (setq web-mode-indent-style 2)
+  (setq web-mode-markup-indent-offset 2)
+  (add-hook 'web-mode-hook 'company-mode))
+
+(use-package css-mode :ensure t
+  :bind
+  (:map css-mode-map
+        ("C-c C-i" . my-web-mode-indent-buffer))
+  :config
+  (add-hook 'css-mode-hook 'company-mode))
+
+(use-package sass-mode :ensure t
+  :mode "\\.sass\\'"
+  :bind
+  (:map css-mode-map
+        ("C-c C-i" . my-web-mode-indent-buffer))
+  :config
+  (add-hook 'sass-mode-hook 'company-mode))
+
+(use-package scss-mode :ensure t
+  :mode "\\.scss\\'"
+  :bind
+  (:map css-mode-map
+        ("C-c C-i" . my-web-mode-indent-buffer))
+  :config
+  (add-hook 'scss-mode-hook 'company-mode))
