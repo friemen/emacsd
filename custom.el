670bab6c623ed601eaa804f83c1771ac90dd2ec3
Falko Riemenschneider <riemenschneider@doctronic.de>
2022-05-17 14:41:42 +0200

    Fix some more colors to adhere to Nord theme

--- custom.el
+++ custom.el
@@ -1,14 +1,21 @@
 
-
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(cider-default-cljs-repl "(user/fig-repl)")
+ '(cider-font-lock-dynamically '(macro function var core))
+ '(cider-print-fn 'fipp)
+ '(cider-repl-display-help-banner nil)
+ '(cider-repl-prompt-function 'cider-repl-prompt-abbreviated)
+ '(cider-save-file-on-load nil)
  '(custom-safe-themes
    '("3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "9e54a6ac0051987b4296e9276eecc5dfb67fdcd620191ee553f40a9b6d943e78" "1157a4055504672be1df1232bed784ba575c60ab44d8e6c7b3800ae76b42f8bd" "cf08ae4c26cacce2eebff39d129ea0a21c9d7bf70ea9b945588c1c66392578d1" "5ee12d8250b0952deefc88814cf0672327d7ee70b16344372db9460e9a0e3ffc" "52588047a0fe3727e3cd8a90e76d7f078c9bd62c0b246324e557dfa5112e0d0c" default))
  '(package-selected-packages
-   '(window-number markdown-mode+ scss-mode nxml-mode cider buffer-move smartrep window-numbering web-mode switch-window smex smart-mode-line sass-mode restclient rainbow-delimiters projectile paxedit nrepl-eval-sexp-fu neotree move-text markdown-mode magit js2-mode impatient-mode ido-vertical-mode ido-ubiquitous highlight-symbol gandalf-theme flycheck flx-ido expand-region emms elscreen dired+ crm-custom counsel company clomacs clojure-quick-repls clj-refactor cider-eval-sexp-fu browse-kill-ring auto-complete ace-jump-mode ace-jump-buffer)))
+   '(window-number markdown-mode+ scss-mode nxml-mode cider buffer-move smartrep window-numbering web-mode switch-window smex smart-mode-line sass-mode restclient rainbow-delimiters projectile paxedit nrepl-eval-sexp-fu neotree move-text markdown-mode magit js2-mode impatient-mode ido-vertical-mode ido-ubiquitous highlight-symbol gandalf-theme flycheck flx-ido expand-region emms elscreen dired+ crm-custom counsel company clomacs clojure-quick-repls clj-refactor cider-eval-sexp-fu browse-kill-ring auto-complete ace-jump-mode ace-jump-buffer))
+ '(sml/no-confirm-load-theme t)
+ '(sml/theme 'light))
 
 
 (custom-set-faces
@@ -17,19 +24,28 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(default ((t (:family "DejaVu Sans Mono" :foundry "unknown" :slant normal :weight normal :height 98 :width normal))))
+ '(cider-test-error-face ((t (:background "#d08770"))))
+ '(cider-test-failure-face ((t (:background "#bf616a"))))
+ '(cider-test-success-face ((t (:background "#a3be8c" :foreground "black"))))
  '(cursor ((t (:background "#E65C39"))))
- '(diredp-dir-name ((t (:foreground "#7474FFFFFFFF"))))
+ '(dired-broken-symlink ((t (:background "#bf616a" :foreground "#ebcb8b" :weight bold))))
+ '(diredp-dir-heading ((t (:foreground "#b48ead"))))
+ '(diredp-dir-name ((t (:foreground "#ebcb8b"))))
+ '(diredp-file-name ((t (:foreground "#d8dee9"))))
+ '(diredp-file-suffix ((t (:foreground "#e5e9f0"))))
  '(diredp-flag-mark-line ((t nil)))
- '(highlight ((t (:background "#81a1c1"))))
+ '(highlight ((t (:background "#81a1c1" :foreground "#2e3440"))))
  '(highlight-symbol-face ((t (:background "gray30"))))
  '(linum ((t :background "#4A8986" :foreground "light gray")))
  '(magit-diff-added ((t (:extend t :background "#a3be8c" :foreground "#2e3440"))))
  '(magit-diff-added-highlight ((t (:extend t :background "#a3be8c" :foreground "#2e3440"))))
  '(magit-diff-removed ((t (:extend t :background "#bf616a" :foreground "#2e3440"))))
  '(magit-diff-removed-highlight ((t (:extend t :background "#bf616a" :foreground "#2e3440"))))
- '(magit-item-highlight ((t (:background "gainsboro"))))
  '(markdown-code-face ((t (:inherit nil :foreground "dark gray"))))
- '(mode-line ((t :foreground "#ffffff" :background "#81a1c1" :inverse-video nil)))
- '(mode-line-inactive ((t :foreground "grey20" :background "Gainsboro" :inverse-video nil)))
- '(region ((t (:background "#7DB0BF" :foreground "#ffffff"))))
+ '(mode-line ((t :foreground "#2e3440" :background "#81a1c1" :inverse-video nil)))
+ '(mode-line-inactive ((t :foreground "#2e3440" :background "#d8dee9" :inverse-video nil)))
+ '(region ((t (:extend t :background "#7DB0BF" :foreground "#2e3440"))))
+ '(sml/filename ((t (:inherit sml/global :foreground "#2e3440" :weight bold))))
+ '(sml/modified ((t (:inherit sml/not-modified :foreground "#bf616a" :weight bold))))
+ '(sml/read-only ((t (:inherit sml/not-modified :foreground "#4c566a" :weight bold))))
  '(treemacs-root-face ((t (:inherit font-lock-constant-face :weight bold :height 1.0)))))
