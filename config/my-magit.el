1c33603bfdb97afd3d7195cca3854ee9c29cd92d
Falko Riemenschneider <riemenschneider@doctronic.de>
2022-05-19 17:08:49 +0200

    Better support for Gerrit in magit

--- config/my-magit.el
+++ config/my-magit.el
@@ -2,6 +2,20 @@
 
 (use-package fullframe :ensure t)
 
+
+(defun magit-push-to-gerrit-local-branch-or-commit (source)
+  "Push an arbitrary branch or commit to Gerrit server. The source is read in the minibuffer."
+  (interactive
+   (let ((source (magit-read-local-branch-or-commit "Push")))
+     (list source)))
+  (magit-git-command-topdir (concat "git push origin " source (concat ":refs/for/" source))))
+
+(defun magit-push-to-gerrit-current-branch ()
+  "Push current branch to Gerrit server."
+  (interactive)
+  (magit-push-to-gerrit-local-branch-or-commit (magit-get-current-branch)))
+
+
 (use-package magit :ensure t
   :bind
   (("C-x g" . magit-status)
@@ -18,7 +32,10 @@
 
   (transient-append-suffix 'magit-push "e"
     '("P" "Push implicitly" magit-push-implicitly))
+  (transient-append-suffix 'magit-push "e"
+    '("g" "Push current branch to Gerrit" magit-push-to-gerrit-current-branch))
+  (transient-append-suffix 'magit-push "e"
+    '("G" "Push a branch to Gerrit" magit-push-to-gerrit-local-branch-or-commit))
 
   (fullframe magit-status magit-mode-quit-window)
-
   )
