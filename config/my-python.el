6356bc0612ad1cb459e88b6e7dfccf075c557b73
Falko Riemenschneider <riemenschneider@doctronic.de>
2025-03-27 21:23:31 +0100

    Improve Python/elpy experience

--- config/my-python.el
+++ config/my-python.el
@@ -5,31 +5,41 @@
   (with-current-buffer (process-buffer (elpy-shell-get-or-create-process))
     (comint-clear-buffer)))
 
+(defun my-elpy-shell-kill-buffer ()
+  (interactive)
+  (elpy-shell-kill)
+  (kill-this-buffer))
+
+(defun my-elpy-system-reload ()
+  (interactive)
+  (python-shell-send-string "import system\nsystem.reload()"))
+
 (use-package elpy :ensure t :defer t
   :bind
   (:map elpy-mode-map
+        ("C-c C-c" . elpy-shell-send-top-statement)
         ("C-c C-x" . my-elpy-shell-clear-buffer)
+        ("C-c C-k" . elpy-shell-send-buffer)
+        ("C-c M-c" . run-python)
+        ("C-c r" . my-elpy-system-reload)
    :map inferior-python-mode-map
-        ("C-c C-x" . my-elpy-shell-clear-buffer))
+        ("C-c C-x" . my-elpy-shell-clear-buffer)
+        ("C-c C-q" . my-elpy-shell-kill-buffer))
 
   :init
   (advice-add 'python-mode :before 'elpy-enable)
 
   :config
+  (require 'eval-sexp-fu)
   (setq python-shell-interpreter "python3"
         python-shell-interpreter-args "-i")
+  (setq elpy-shell-echo-input nil)
+  (add-hook 'elpy-mode-hook 'highlight-symbol-mode)
+  (add-hook 'elpy-mode-hook 'eval-sexp-fu-flash-mode)
   (font-lock-add-keywords 'python-mode my-todo-keywords))
 
 
-(use-package pyvenv :ensure t
+(use-package auto-virtualenv :ensure t
   :config
-  (pyvenv-mode t)
-
-  ;; set correct Python interpreter, after venv is selected
-  (setq pyvenv-post-activate-hooks
-        (list (lambda ()
-                (message "Using venv %s" pyvenv-virtual-env)
-                (setq python-shell-interpreter (concat pyvenv-virtual-env "bin/python3")))))
-  (setq pyvenv-post-deactivate-hooks
-        (list (lambda ()
-                (setq python-shell-interpreter "python3")))))
+  (setq auto-virtualenv-verbose nil)
+  (auto-virtualenv-setup))
